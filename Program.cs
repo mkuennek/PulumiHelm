8ee5d135705f8e384a54d8ca84dd37fc962445b6
Michael Künneke <m.kuenneke@googlemail.com>
2024-09-05 15:41:58 +0200

    Use empty curly braces

--- Program.cs
+++ Program.cs
@@ -15,7 +15,58 @@ return await Deployment.RunAsync(() =>
             {
                 Repo = "https://open-telemetry.github.io/opentelemetry-helm-charts"
             },
-            ValueYamlFiles = new FileAsset("./OtelCollector.yaml")
+            ValueYamlFiles = new FileAsset("./OtelCollector.yaml"),
+            // Values = new InputMap<object>
+            // {
+            //     ["mode"] = "deployment",
+            //     ["image"] = new InputMap<object>
+            //     {
+            //         ["repository"] = "otel/opentelemetry-collector-contrib",
+            //     },
+            //     ["config"] = new InputMap<object>
+            //     {
+            //         ["receivers"] = new InputMap<object>
+            //         {
+            //             ["jaeger"] = new InputMap<object>(),
+            //             ["prometheus"] = new InputMap<object>(),
+            //             ["zipkin"] = new InputMap<object>(),
+            //         },
+            //         ["service"] = new InputMap<object>
+            //         {
+            //             ["pipelines"] = new InputMap<object>
+            //             {
+            //                 ["logs"] = new InputMap<object>(),
+            //                 ["metrics"] = new InputMap<object>(),
+            //                 ["traces"] = new InputMap<object>
+            //                 {
+            //                     ["receivers"] = new InputList<string>{ "otlp" },
+            //                     ["exporters"] = new InputList<string>{ "debug" },
+            //                 },
+            //
+            //             },
+            //         }
+            //     }
+            //     ["debug"] = new InputMap<object>
+            //     {
+            //         ["jaeger-compact"] = new InputMap<object>
+            //         {
+            //             ["enabled"] = false,
+            //         },
+            //         ["jaeger-thrift"] = new InputMap<object>
+            //         {
+            //             ["enabled"] = false,
+            //         },
+            //         ["jaeger-grpc"] = new InputMap<object>
+            //         {
+            //             ["enabled"] = false,
+            //         },
+            //         ["zipkin"] = new InputMap<object>
+            //         {
+            //             ["enabled"] = false,
+            //         },
+            //     }
+            // },
+            AllowNullValues = true,
         });
 
     return new Dictionary<string, object?>();
